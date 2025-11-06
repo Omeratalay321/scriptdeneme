fc77123ffaf41019fe3f41f09dddc6bfed179f15
Omeratalay321 <ossuransinek123@gmail.com>
2025-11-06 21:36:22 +0300

    script1.js

---
+++ script1.js
@@ -0,0 +1,7 @@
+// script1.js dosyasının içeriği:
+
+// 1. Ortak kullanacağımız bir değişken tanımlıyoruz.
+let toplanan_deger = 100;
+
+// 2. Birinci script'in çalıştığını göstermek için HTML'e yazı yazıyoruz.
+document.getElementById('sonuc').innerHTML = "Script 1 çalıştı. Başlangıç değeri: " + toplanan_deger;
